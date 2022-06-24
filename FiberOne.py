424ba0f56fc085027c238292ec8f1e5242674ad8
Alex Hillberry <AlexHillberry@gmail.com>
2022-06-24 01:04:26 -0400

    Modified program to include ranges for discounts and created title and end screen modifications.

--- FiberOne.py
+++ FiberOne.py
@@ -1,13 +1,15 @@
 def main():
     # Welcome
-    print("Welcome, and Thank You for using FiberOne Installations")
+    print("==========================================================")
+    print("Welcome! Thank You for using FiberOne Installations.")
+    print("==========================================================")
     # Enter Company_Name
     naming = True
     company_name = ""
     while naming:
         company_name = (input("Please enter your company name to begin \n"))
-        if company_name.isalpha():
-            print(company_name + ", Go ahead with your order")
+        if company_name.isalnum():
+            print(company_name + ", Go ahead with your order!")
             naming = False
         else:
             print("You must enter alpha-numeric characters only \n")
@@ -15,14 +17,22 @@ def main():
     shopping = True
     while shopping:
         cable_length = int(input("Please enter the amount of cable to be installed in feet \n"))
-        if cable_length >= 100:
-            print("Thank you", company_name, ",Your total expense is $", cable_length * 0.7)
-        if cable_length < 100:
+        if cable_length <= 100:
             print("Thank you", company_name, ",Your total expense is $", cable_length * 0.87)
+        elif 100 < cable_length <= 250:
+            print("Thank you", company_name, ",Your total expense is $", cable_length * 0.8)
+        elif 250 < cable_length <= 500:
+            print("Thank you", company_name, ",Your total expense is $", cable_length * 0.7)
+        elif cable_length > 500:
+            print("Thank you", company_name, ",Your total expense is $", cable_length * 0.5)
         continue_purchase = input("Would you like to place another order Y/N \n")
-        if continue_purchase == "N":
-            shopping = False
-    print("Thank you for using FiberOne Installations!")
+        while continue_purchase.lower() not in ("y", "n"):
+            continue_purchase = input("You must type Y/N \n")
+        if continue_purchase.lower() == "n":
+            break
+    print("==========================================================")
+    print("Thank you for using FiberOne Installations! ☺")
+    print("==========================================================")
 
 
 # where the code starts
